36db2bee09d05d5456ec1d2b5148d59434c9e1c8
oliverrees <5574826+oliverrees@users.noreply.github.com>
2024-08-29 17:48:55 +0100

    Screenshot and badge

--- site/src/app/map/[mapSlug]/page.tsx
+++ site/src/app/map/[mapSlug]/page.tsx
@@ -38,7 +38,7 @@ export async function generateMetadata({
     image: mapDetails?.screenshot_url || "",
     openGraph: {
       title: mapDetails?.map_name || "Map not found",
-      description: `View the map ${mapDetails?.map_name} on ExploreMap`,
+      description: `Discover stats, weather, photos and more from ${mapDetails?.map_name}`,
       url: "https://exploremap.io",
       siteName: "ExploreMap",
       images: [
